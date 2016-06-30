0b0d3e8fbc4c57945633a165b34bf09be3a3a683
Yunhao Shen <xiaohao9006@gmail.com>
2016-06-29 20:14:42 -0700

    Work Projects Started
    Work Projects just started

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,85 +18,83 @@ angular.module('projectApp')
   })*/
 
   .controller('AppController',['$scope', function($scope){
-    $scope.ngViewLoad = 0
     $scope.onViewLoad = function(){
       //console.log('view changed');
-      if ($scope.ngViewLoad === 0)
+
+      var srollMagicController = new ScrollMagic.Controller();
+      var entryScene = new ScrollMagic.Scene({
+        //triggerElement: "#entryPage",
+        offset: 1
+      })
+        .addTo(srollMagicController);
+
+      entryScene.on("enter", function()
       {
-        var srollMagicController = new ScrollMagic.Controller();
-        var entryScene = new ScrollMagic.Scene({
-          //triggerElement: "#entryPage",
-          offset: 1
-        })
-          .addTo(srollMagicController);
+        //console.log("asdasd");
+        $("#menuHeader").css("background", "#2d2c33");
 
-        entryScene.on("enter", function()
-        {
-          //console.log("asdasd");
-          $("#menuHeader").css("background", "#2d2c33");
+      });
+      entryScene.on("leave", function()
+      {
+        $("#menuHeader").css("background", "none");
 
+      });
+
+      var experienceScene = new ScrollMagic.Scene({
+        triggerElement: "#experienceTrigger",
+        //offset:100
+      })
+        .addTo(srollMagicController);
+
+      experienceScene.on("enter", function()
+      {
+        //console.log("!!!!!!");
+        $("#flightToUSA").addClass("animated fadeInUp");
+        $("#graduation").addClass("animated fadeInDown");
+        $("#executiveDashboard").addClass("animated fadeInDown");
+        $("#adobeReports").addClass("animated fadeInUp");
+        $("#myAgencyReports").addClass("animated fadeInDown");
+
+
+      });
+
+      var skillsScene = new ScrollMagic.Scene({
+        triggerElement: "#skillsTrigger"
+        //offset:
+      })
+        .addTo(srollMagicController);
+
+      skillsScene.on("enter", function(){
+        $("#skillsBar-1").addClass("animated growBar");
+        $("#skillsText-1").addClass("animated fadeIn");
+        $("#skillsIcon-1").addClass("animated fadeIn");
+        $("#skillsBar-2").addClass("animated growBar ");
+        $("#skillsText-2").addClass("animated fadeIn ");
+        $("#skillsIcon-2").addClass("animated fadeIn ");
+        $("#skillsBar-3").addClass("animated growBar ");
+        $("#skillsText-3").addClass("animated fadeIn ");
+        $("#skillsIcon-3").addClass("animated fadeIn ");
+        $("#skillsBar-4").addClass("animated growBar ");
+        $("#skillsText-4").addClass("animated fadeIn ");
+        $("#skillsIcon-4").addClass("animated fadeIn ");
+        $("#skillsBar-5").addClass("animated growBar ");
+        $("#skillsText-5").addClass("animated fadeIn ");
+        $("#skillsIcon-5").addClass("animated fadeIn ");
+
+        $("#English").addClass("animated show75");
+        $("#Chinese").addClass("animated show100");
+        $("#Spanish").addClass("animated show25");
+
+      });
+
+      $("#learnMore").click(function(){
+        srollMagicController.scrollTo(function(learnMoreScrollToAnchor){
+          $('html, body').animate({scrollTop : learnMoreScrollToAnchor},800);
         });
-        entryScene.on("leave", function()
-        {
-          $("#menuHeader").css("background", "none");
+        srollMagicController.scrollTo("#learnMoreScrollToAnchor");
 
-        });
+      })
 
-        var experienceScene = new ScrollMagic.Scene({
-          triggerElement: "#experienceTrigger",
-          //offset:100
-        })
-          .addTo(srollMagicController);
-
-        experienceScene.on("enter", function()
-        {
-          //console.log("!!!!!!");
-          $("#flightToUSA").addClass("animated fadeInUp");
-          $("#graduation").addClass("animated fadeInDown delay");
-          $("#executiveDashboard").addClass("animated fadeInDown delay-one");
-          $("#adobeReports").addClass("animated fadeInUp delay-two");
-          $("#myAgencyReports").addClass("animated fadeInDown delay-three");
-
-
-        });
-
-        var skillsScene = new ScrollMagic.Scene({
-          triggerElement: "#skillsTrigger"
-          //offset:
-        })
-          .addTo(srollMagicController);
-
-        skillsScene.on("enter", function(){
-          $("#skillsBar-1").addClass("animated growBar");
-          $("#skillsText-1").addClass("animated fadeIn");
-          $("#skillsIcon-1").addClass("animated fadeIn");
-          $("#skillsBar-2").addClass("animated growBar delay");
-          $("#skillsText-2").addClass("animated fadeIn delay");
-          $("#skillsIcon-2").addClass("animated fadeIn delay");
-          $("#skillsBar-3").addClass("animated growBar delay-one");
-          $("#skillsText-3").addClass("animated fadeIn delay-one");
-          $("#skillsIcon-3").addClass("animated fadeIn delay-one");
-          $("#skillsBar-4").addClass("animated growBar delay-two");
-          $("#skillsText-4").addClass("animated fadeIn delay-two");
-          $("#skillsIcon-4").addClass("animated fadeIn delay-two");
-          $("#skillsBar-5").addClass("animated growBar delay-three");
-          $("#skillsText-5").addClass("animated fadeIn delay-three");
-          $("#skillsIcon-5").addClass("animated fadeIn delay-three");
-
-          $("#English").addClass("animated show75");
-          $("#Chinese").addClass("animated show100");
-          $("#Spanish").addClass("animated show25");
-
-        });
-
-        $("#learnMore").click(function(){
-          srollMagicController.scrollTo(function(learnMoreScrollToAnchor){
-            $('html, body').animate({scrollTop : learnMoreScrollToAnchor},800);
-          });
-          srollMagicController.scrollTo("#learnMoreScrollToAnchor");
-        })
-        $scope.ngViewLoad++;
-      }
 
 
 
